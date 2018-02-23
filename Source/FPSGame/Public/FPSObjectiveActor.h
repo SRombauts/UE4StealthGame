934adf2705bac604b3c8e2d2e3ee32f48b414af9
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2018-02-23 20:08:53 +0100

    Add FPSObjectiveActor with StaticMesh and Sphere components, no hit collision, only query overlap

---
+++ Source/FPSGame/Public/FPSObjectiveActor.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSObjectiveActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class FPSGAME_API AFPSObjectiveActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFPSObjectiveActor();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* SphereComp;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
